45c8994cc668477da684ecc14da082927c37c4df
Ashley Holmes <ashleyholmes@gmail.com>
2017-03-20 02:41:41 +0000

    fifth commit
    Symfony Form Types

--- src/AppBundle/Form/UserType.php
+++ src/AppBundle/Form/UserType.php
@@ -3,6 +3,9 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +16,47 @@ class UserType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('username')->add('firstname')->add('lastname')->add('email')->add('password')        ;
+        $builder->add('username', TextType::class, array(
+            'label' => "Username",
+            'required' => true,
+            'attr' => array(
+                'placeholder' => "Enter Username",
+                'class' => "form-control"
+            )
+        ))
+            ->add('firstname', TextType::class, array(
+                'label' => "First Name",
+                'required' => true,
+                'attr' => array(
+                    'placeholder' => "Enter Firstname",
+                    'class' => "form-control"
+                )
+            ))
+            ->add('lastname', TextType::class, array(
+                'label' => "Last Name",
+                'required' => true,
+                'attr' => array(
+                    'placeholder' => "Enter Last Name",
+                    'class' => "form-control"
+                )
+            ))
+            ->add('email', EmailType::class, array(
+                'label' => "Email",
+                'required' => true,
+                'attr' => array(
+                    'placeholder' => "Enter Email",
+                    'class' => "form-control"
+                )
+            ))
+            ->add('password', PasswordType::class, array(
+                'label' => "Password",
+                'required' => true,
+                'attr' => array(
+                    'placeholder' => "Enter Password",
+                    'class' => "form-control"
+                )
+            ));
+        /*$builder->add('username')->add('firstname')->add('lastname')->add('email')->add('password');*/
     }
     
     /**
