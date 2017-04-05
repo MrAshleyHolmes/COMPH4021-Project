98bc7d6eda892883ebb9d1141e213d11c19fcbae
Ashley Holmes <ashleyholmes@gmail.com>
2017-04-05 17:27:33 +0100

    fourteenth commit
    Extending the Login Form

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -19,10 +19,10 @@ class SecurityController extends Controller
         $error = $authenticationUtils->getLastAuthenticationError();
 
         // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
+        /*$lastUsername = $authenticationUtils->getLastUsername();*/
 
         return $this->render('security/login.html.twig', array(
-            'last_username' => $lastUsername,
+            /*'last_username' => $lastUsername,*/
             'error'         => $error,
         ));
     }
--- var/cache/dev/twig/0f/0f55ae0027086149782a2b8a6229da6cebe69b6877ca77df353d34a70c300fe3.php
+++ var/cache/dev/twig/0f/0f55ae0027086149782a2b8a6229da6cebe69b6877ca77df353d34a70c300fe3.php
@@ -21,29 +21,29 @@ class __TwigTemplate_12f9825b1d022b39d19cebfa5445879f7fc0ec3ecbfb7092e4f368a7cdc
 
     protected function doDisplay(array $context, array $blocks = array())
     {
-        $__internal_0932400b5061cc52d3009991db30f2b81d4c1429e5f70eb35d16f2be5888eafb = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
-        $__internal_0932400b5061cc52d3009991db30f2b81d4c1429e5f70eb35d16f2be5888eafb->enter($__internal_0932400b5061cc52d3009991db30f2b81d4c1429e5f70eb35d16f2be5888eafb_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "security/login.html.twig"));
+        $__internal_70b60ec5c964c61a8c051c9b9176bc77984ccf20d203eec0226accf6d61e7ec7 = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_70b60ec5c964c61a8c051c9b9176bc77984ccf20d203eec0226accf6d61e7ec7->enter($__internal_70b60ec5c964c61a8c051c9b9176bc77984ccf20d203eec0226accf6d61e7ec7_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "security/login.html.twig"));
 
-        $__internal_7abcf9d546839c0a3808066198ceb9871190b5d75a2518b98b325f17287e5a68 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
-        $__internal_7abcf9d546839c0a3808066198ceb9871190b5d75a2518b98b325f17287e5a68->enter($__internal_7abcf9d546839c0a3808066198ceb9871190b5d75a2518b98b325f17287e5a68_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "security/login.html.twig"));
+        $__internal_3e53789e3b0368e09f50371f84da60063e7e6bb13f35523659e610137ff3d71d = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_3e53789e3b0368e09f50371f84da60063e7e6bb13f35523659e610137ff3d71d->enter($__internal_3e53789e3b0368e09f50371f84da60063e7e6bb13f35523659e610137ff3d71d_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "security/login.html.twig"));
 
         $this->parent->display($context, array_merge($this->blocks, $blocks));
         
-        $__internal_0932400b5061cc52d3009991db30f2b81d4c1429e5f70eb35d16f2be5888eafb->leave($__internal_0932400b5061cc52d3009991db30f2b81d4c1429e5f70eb35d16f2be5888eafb_prof);
+        $__internal_70b60ec5c964c61a8c051c9b9176bc77984ccf20d203eec0226accf6d61e7ec7->leave($__internal_70b60ec5c964c61a8c051c9b9176bc77984ccf20d203eec0226accf6d61e7ec7_prof);
 
         
-        $__internal_7abcf9d546839c0a3808066198ceb9871190b5d75a2518b98b325f17287e5a68->leave($__internal_7abcf9d546839c0a3808066198ceb9871190b5d75a2518b98b325f17287e5a68_prof);
+        $__internal_3e53789e3b0368e09f50371f84da60063e7e6bb13f35523659e610137ff3d71d->leave($__internal_3e53789e3b0368e09f50371f84da60063e7e6bb13f35523659e610137ff3d71d_prof);
 
     }
 
     // line 3
     public function block_body($context, array $blocks = array())
     {
-        $__internal_44fad36f0695df4ddc742fcd8be18dc77a49013d05ca57527e281c944af6af55 = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
-        $__internal_44fad36f0695df4ddc742fcd8be18dc77a49013d05ca57527e281c944af6af55->enter($__internal_44fad36f0695df4ddc742fcd8be18dc77a49013d05ca57527e281c944af6af55_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+        $__internal_2d4ad57c672eddee9954782a651feb933f03adb469b4e237c6f6112b24186b77 = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_2d4ad57c672eddee9954782a651feb933f03adb469b4e237c6f6112b24186b77->enter($__internal_2d4ad57c672eddee9954782a651feb933f03adb469b4e237c6f6112b24186b77_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
 
-        $__internal_0e04bd91e0e6f42a6ac04b9d287326837afe027a44f4a55a34e09a1078018cba = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
-        $__internal_0e04bd91e0e6f42a6ac04b9d287326837afe027a44f4a55a34e09a1078018cba->enter($__internal_0e04bd91e0e6f42a6ac04b9d287326837afe027a44f4a55a34e09a1078018cba_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+        $__internal_4dd6a9bfe8d6f7aa354cffb070635495c47c0c089418a90071c70d7c71b6c942 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_4dd6a9bfe8d6f7aa354cffb070635495c47c0c089418a90071c70d7c71b6c942->enter($__internal_4dd6a9bfe8d6f7aa354cffb070635495c47c0c089418a90071c70d7c71b6c942_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
 
         // line 4
         echo "    <div class=\"jumbotron\">
@@ -54,12 +54,10 @@ class __TwigTemplate_12f9825b1d022b39d19cebfa5445879f7fc0ec3ecbfb7092e4f368a7cdc
         // line 8
         if (($context["error"] ?? $this->getContext($context, "error"))) {
             // line 9
-            echo "        ";
-            // line 10
-            echo "        <div>";
-            echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\TranslationExtension')->trans($this->getAttribute(($context["error"] ?? $this->getContext($context, "error")), "messageKey", array()), $this->getAttribute(($context["error"] ?? $this->getContext($context, "error")), "messageData", array()), "security"), "html", null, true);
-            echo "</div>
-    ";
+            echo "        <div class=\"alert alert-danger\" role=\"alert\">We have no match for that user and password</div>
+        ";
+            // line 11
+            echo "    ";
         }
         // line 12
         echo "    <div class=\"form\">
@@ -68,15 +66,13 @@ class __TwigTemplate_12f9825b1d022b39d19cebfa5445879f7fc0ec3ecbfb7092e4f368a7cdc
         echo $this->env->getExtension('Symfony\Bridge\Twig\Extension\RoutingExtension')->getPath("login_check");
         echo "\" method=\"post\">
             <div class=\"form-group\">
+                <label for=\"email\">Username or Email:</label>
                 ";
-        // line 16
-        echo "                <label for=\"username\">Username:</label>
+        // line 17
+        echo "                <input type=\"text\" id=\"email\" name=\"_email\"
                 ";
-        // line 18
-        echo "                <input type=\"text\" id=\"username\" name=\"_username\" value=\"";
-        echo twig_escape_filter($this->env, ($context["last_username"] ?? $this->getContext($context, "last_username")), "html", null, true);
-        echo "\"
-                       class=\"form-control\"/>
+        // line 19
+        echo "                       class=\"form-control\"/>
             </div>
             <div class=\"form-group\">
                 <label for=\"password\">Password:</label>
@@ -91,10 +87,10 @@ class __TwigTemplate_12f9825b1d022b39d19cebfa5445879f7fc0ec3ecbfb7092e4f368a7cdc
     </div>
 ";
         
-        $__internal_0e04bd91e0e6f42a6ac04b9d287326837afe027a44f4a55a34e09a1078018cba->leave($__internal_0e04bd91e0e6f42a6ac04b9d287326837afe027a44f4a55a34e09a1078018cba_prof);
+        $__internal_4dd6a9bfe8d6f7aa354cffb070635495c47c0c089418a90071c70d7c71b6c942->leave($__internal_4dd6a9bfe8d6f7aa354cffb070635495c47c0c089418a90071c70d7c71b6c942_prof);
 
         
-        $__internal_44fad36f0695df4ddc742fcd8be18dc77a49013d05ca57527e281c944af6af55->leave($__internal_44fad36f0695df4ddc742fcd8be18dc77a49013d05ca57527e281c944af6af55_prof);
+        $__internal_2d4ad57c672eddee9954782a651feb933f03adb469b4e237c6f6112b24186b77->leave($__internal_2d4ad57c672eddee9954782a651feb933f03adb469b4e237c6f6112b24186b77_prof);
 
     }
 
@@ -110,7 +106,7 @@ class __TwigTemplate_12f9825b1d022b39d19cebfa5445879f7fc0ec3ecbfb7092e4f368a7cdc
 
     public function getDebugInfo()
     {
-        return array (  88 => 31,  76 => 18,  73 => 16,  68 => 13,  65 => 12,  59 => 10,  57 => 9,  55 => 8,  49 => 4,  40 => 3,  11 => 1,);
+        return array (  84 => 31,  75 => 19,  72 => 17,  66 => 13,  63 => 12,  60 => 11,  57 => 9,  55 => 8,  49 => 4,  40 => 3,  11 => 1,);
     }
 
     /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
@@ -131,16 +127,16 @@ class __TwigTemplate_12f9825b1d022b39d19cebfa5445879f7fc0ec3ecbfb7092e4f368a7cdc
     </div>
 
     {% if error %}
-        {#<div class=\"alert alert-danger\" role=\"alert\">We have no match for that user and password</div>#}
-        <div>{{ error.messageKey|trans(error.messageData, 'security') }}</div>
+        <div class=\"alert alert-danger\" role=\"alert\">We have no match for that user and password</div>
+        {#<div>{{ error.messageKey|trans(error.messageData, 'security') }}</div>#}
     {% endif %}
     <div class=\"form\">
         <form action=\"{{ path('login_check') }}\" method=\"post\">
             <div class=\"form-group\">
-                {#<label for=\"username\">Username or Email:</label>#}
-                <label for=\"username\">Username:</label>
-                {#<input type=\"text\" id=\"email\" name=\"_email\"#}
-                <input type=\"text\" id=\"username\" name=\"_username\" value=\"{{ last_username }}\"
+                <label for=\"email\">Username or Email:</label>
+                {#<label for=\"username\">Username:</label>#}
+                <input type=\"text\" id=\"email\" name=\"_email\"
+                {#<input type=\"text\" id=\"username\" name=\"_username\" value=\"{{ last_username }}\"#}
                        class=\"form-control\"/>
             </div>
             <div class=\"form-group\">
